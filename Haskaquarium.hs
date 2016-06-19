9dd7501611d02fbdd9556fed5e03a6d57a8c36c4
LudoBike <ludovicdruette@gmail.com>
2016-06-19 11:50:44 +0200

    Set cabal project and write trivial main function

---
+++ Haskaquarium.hs
@@ -0,0 +1,14 @@
+--
+--         Haskaquarium
+-- For informations on the project itself see README.md
+-- For informations on the license see License.md
+--
+
+module Main where
+
+import System.IO
+
+-- 'main' runs the main program
+main :: IO ()
+main = putStrLn "Hello World !"
+
