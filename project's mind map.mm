2d486356b0338fab44768fbe926f955e3c16b0e9
LudoBike <ludovicdruette@gmail.com>
2016-06-19 11:23:45 +0200

    Add primitive mind map for exercice 1.1

---
+++ project's mind map.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1466326119608" ID="ID_1055019596" MODIFIED="1466327255456" TEXT="Haskaquarium">
+<node CREATED="1466326345329" ID="ID_298606579" MODIFIED="1466326903023" POSITION="right" TEXT="Aquarium">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1466326541899" HGAP="29" ID="ID_1786728697" MODIFIED="1466326880342" TEXT="List of Fish" VSHIFT="-60">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1466326804019" HGAP="31" ID="ID_588401830" MODIFIED="1466326880341" TEXT="addFish" VSHIFT="-52">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1466326670796" HGAP="22" ID="ID_1164713396" MODIFIED="1466326922697" TEXT="Number of seaweed" VSHIFT="63">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1466326926428" HGAP="65" ID="ID_812366006" MODIFIED="1466326961790" TEXT="Integral or List of Seaweed" VSHIFT="76">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1466326984114" HGAP="34" ID="ID_466952263" MODIFIED="1466327222420" TEXT="main" VSHIFT="37">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1466326373836" HGAP="42" ID="ID_1371045112" MODIFIED="1466326903023" POSITION="left" TEXT="Fish" VSHIFT="1">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1466326703206" HGAP="21" ID="ID_1835306381" MODIFIED="1466326903023" TEXT="name" VSHIFT="-13">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1466326727724" HGAP="25" ID="ID_1309140507" MODIFIED="1466326880339" TEXT="sexe" VSHIFT="18">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1466326396983" HGAP="-65" ID="ID_1301489268" MODIFIED="1466326892920" POSITION="right" TEXT="Seaweed" VSHIFT="58">
+<icon BUILTIN="help"/>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1466326744905" HGAP="32" ID="ID_792872093" MODIFIED="1466326880338" POSITION="left" TEXT="Sexe" VSHIFT="62">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1466326758802" ID="ID_92086487" MODIFIED="1466326880337" TEXT="Male" VSHIFT="-17">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1466326764776" HGAP="17" ID="ID_738737348" MODIFIED="1466326880336" TEXT="Female" VSHIFT="19">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+</map>
